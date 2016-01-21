31577182c126264e26bef8140346b1cf16d81144
Haroen Viaene <hello@haroen.me>
2016-01-21 21:45:17 +0100

    cleaner

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ window.addEventListener('DOMContentLoaded',function(){
   var search = document.querySelector('.search input');
   var courseNames = document.querySelectorAll('.repos--list h2 a');
   var courseDescriptions = document.querySelectorAll('.repos--list p');
-  search.addEventListener('input',function(){
 
+  search.addEventListener('input',function(){
     for (var i = 0; i < courseNames.length; i++) {
       if (courseNames[i].innerHTML.toLowerCase().indexOf(search.value.toLowerCase()) === -1 && courseDescriptions[i].innerHTML.toLowerCase().indexOf(search.value.toLowerCase()) === -1) {
         courseNames[i].parentNode.parentNode.classList.add('hidden');
